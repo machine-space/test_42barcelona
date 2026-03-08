3a7fdcf76e30e03c67e50114352b779f80aa8b32
Artem <vonmes87@gmail.com>
2026-03-08 02:39:58 +0100

    Add putnbr with recursion

--- main.c
+++ main.c
@@ -1,32 +1,35 @@
 #include <unistd.h>
 
-void ft_putchar(char c) {
+void ft_putchar(char c)
+{
     write(1, &c, 1);
 }
 
 void ft_putnbr(int nb)
 {
     // 1. We handle the extreme case separately
-    if(nb == -2147483648) {
+    if(nb == -2147483648)
+    {
 	write(1, "-214748648", 11);
 	return;
     }
 
     // 2. Handling regular negative numbers
-    if(nb < 0) {
+    if(nb < 0)
+    {
 	ft_putchar('-');
 	nb = -nb;
     }
 
     // 3. Recursion
-    if(nb > 9) {
+    if(nb > 9)
 	ft_putnbr(nb / 10);
-    }
 
     ft_putchar(nb % 10 + '0');
 }
 
-int main() {
+int main()
+{
     ft_putnbr(42);
     ft_putchar('\n');
     ft_putnbr(-123);
