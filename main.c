511435e111269210ec3ea3470734362c88293a24
Artem <vonmes87@gmail.com>
2026-03-08 02:31:39 +0100

    Add putnbr with recursion

--- main.c
+++ main.c
@@ -1,20 +1,40 @@
 #include <unistd.h>
 
-
-void ft_putstr_recursive(char* str)
+void ft_putchar(char c)
 {
-    if(*str == '\0')
-        return;
+    write(1, &c, 1);
+}
 
-    ft_putstr_recursive(str + 1);
+void ft_putnbr(int nb)
+{
+    // 1. We handle the extreme case separately
+    if(nb == -2147483648)
+    {
+	write(1, "-214748648", 11);
+	return;
+    }
 
-    write(1, str, 1);
+    // 2. Handling regular negative numbers
+    if(nb < 0)
+    {
+	ft_putchar('-');
+	nb = -nb;
+    }
+
+    // 3. Recursion
+    if(nb > 9)
+    {
+	ft_putnbr(nb / 10);
+    }
+
+    ft_putchar(nb % 10 + '0');
 }
 
 int main()
 {
-    char* message = "Hello, Low Lewel Word!\n";
-
-    ft_putstr_recursive(message);
-    return (0);
+    ft_putnbr(42);
+    ft_putchar('\n');
+    ft_putnbr(-123);
+    ft_putchar('\n');
+    return 0;
 }
